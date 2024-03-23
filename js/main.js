077f626b84a3c414810632852c49783da22f2b15
Ezequiel-Front-End <ezequielfernandes912@gmail.com>
2024-03-23 17:10:06 -0300

    adicionando o JavaScript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const myForm = document.getElementById("myForm");
+const sucessSubmit = (nome) => {
+    Swal.fire({
+        title: "Sucesso!",
+        text: `Parabens, ${nome}! Formulario Enviado!`,
+        icon: "success"
+    });
+
+}
+
+myForm.addEventListener('submit', (event) => {
+    let nome = myForm.nome.value
+    sucessSubmit(nome);
+
+    event.preventDefault();
+});
