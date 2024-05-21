c312136587eafe45f56d3d299fda930b9188a83d
dev-abctechnology <dev@abctechnology.com.br>
2024-05-21 13:47:00 -0300

    adicionando validacao de formulario

---
+++ database/insert.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JSP</title>
+</head>
+<body>
+    <% 
+        out.print("Testando a Aplicação!")
+    %>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -8,11 +8,30 @@ const sucessSubmit = (nome) => {
         icon: "success"
     });
 
+
+    setTimeout(() => {
+        window.location.reload();
+
+    }, 2000)
+
 }
 
 myForm.addEventListener('submit', (event) => {
-    let nome = myForm.nome.value
-    sucessSubmit(nome);
+    let nome = myForm.nome.value;
+    let email = myForm.email.value;
+    let mensagem = myForm.mensagem.value;
+    let erro = document.querySelectorAll(".erro");
+
+    nome == "" ? erro[0].style.display = "block" : erro[0].style.display = "none";
+    email == "" ? erro[1].style.display = "block" : erro[1].style.display = "none";
+    mensagem == "" ? erro[2].style.display = "block" : erro[2].style.display = "none";
+
+
+    if (nome != "" && email != "" && mensagem !== "") {
+        sucessSubmit(nome);
+
+    }
 
     event.preventDefault();
+
 });
